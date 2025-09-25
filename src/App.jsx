0b0cdb25c29a193e1747da15b62ee2f058832cb3
MiguelTr7 <167789563+MiguelTr7@users.noreply.github.com>
2025-09-25 15:28:54 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contacto" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
